2caa93440bab058bc258c241f05c84bd9d512e1d
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-01 21:45:16 -0300

    Added security key schema

---
+++ src/entities/securityKey.entity.ts
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs';
+
+export default class SecurityKey {
+  reference: string;
+  key: string;
+
+  constructor(secKey) {
+    this.reference = secKey.reference;
+    this.key = secKey.key;
+  }
+
+  validate() {
+    if (!this.reference || !this.key) {
+      throw new Error('Missing required field(s) for creating key.');
+    }
+  }
+
+  getKey() {
+    return {
+      reference: this.reference,
+      key: bcrypt.hashSync(this.key, 10),
+    };
+  }
+}
