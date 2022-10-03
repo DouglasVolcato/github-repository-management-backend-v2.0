379c396338f9f0a6127fe30081655800c1d68b96
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-03 14:28:08 -0300

    Added middleware to repository routes

--- src/repo/repo.module.ts
+++ src/repo/repo.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { RepoController } from './repo.controller';
 import { RepoService } from './repo.service';
+import { Middleware } from 'src/middleware';
 
 @Module({
   controllers: [RepoController],
   providers: [RepoService],
 })
-export class RepoModule {}
+export class RepoModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(Middleware)
+      .forRoutes(
+        'repo/create-repository',
+        'repo/delete-repository/:name',
+        'repo/get-all-repository',
+        'repo/update-repository/:name',
+      );
+  }
+}
