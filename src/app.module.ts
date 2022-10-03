e5a7b678adeea2bc3ef558da1ab05aa0302b85f8
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-03 14:14:13 -0300

    Added SecurityKey and Repository modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { RepoModule } from './repo/repo.module';
+import { SecurityKeyModule } from './security-key/security-key.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, RepoModule, SecurityKeyModule],
 })
 export class AppModule {}
