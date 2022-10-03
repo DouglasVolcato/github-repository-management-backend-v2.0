a5ad737adaef717c49b2932530873b1aee91b95c
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-03 14:15:51 -0300

    Added SecurityKey service

---
+++ src/security-key/security-key.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import SecurityKey from 'src/entities/securityKey.entity';
+import { SecurityKeyRepository } from 'src/database/repositories/securityKey.repository';
+
+@Injectable()
+export class SecurityKeyService {
+  async create(userEmail, arrKeys) {
+    const arr = [];
+
+    for (const key of arrKeys) {
+      const newKey = new SecurityKey(key);
+      newKey.validate();
+      arr.push({ ...newKey.getKey() });
+    }
+
+    const data = await SecurityKeyRepository.addKeys(userEmail, arr);
+    return data;
+  }
+
+  async editPassword(userEmail, newPassword) {
+    return await SecurityKeyRepository.editPassword(userEmail, newPassword);
+  }
+
+  async getAll(userEmail) {
+    const keys = await SecurityKeyRepository.getKeys(userEmail);
+    return keys.securityKeys;
+  }
+}
