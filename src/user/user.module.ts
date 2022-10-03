5da5e2e258d5a88eabdfd57c203fcb1acb2a0745
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-03 02:50:07 -0300

    Added middleware for routes

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -9,6 +9,14 @@ import { UserService } from './user.service';
 })
 export class UserModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(Middleware).forRoutes('user/delete-user');
+    consumer
+      .apply(Middleware)
+      .forRoutes(
+        'user/delete-user',
+        'user/get-all-user',
+        'user/get-by-email-user/:email',
+        'user/get-by-id-user/:id',
+        'user/update-user',
+      );
   }
 }
