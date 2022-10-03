d3d64e5fbf75b2f57d48ea2681c916bfaa3cec43
Douglas Volcato <douglasvolcato@gmail.com>
2022-10-03 02:56:14 -0300

    Added swagger documentation builder

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MongoDbConnection } from './database/connection/connect';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { config } from 'dotenv';
+config();
 
 async function bootstrap() {
-  config();
   const app = await NestFactory.create(AppModule);
   MongoDbConnection.connectDb();
+
+  const config = new DocumentBuilder()
+    .setTitle('Expense App')
+    .setDescription('CRUD complete for financial management.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   const port = process.env.PORT || 3001;
   await app.listen(port, () => console.log('http://localhost:' + port));
 }
